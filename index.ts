08fd5d10e5726b75dec7476901bfe69437b74307
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-10-30 15:42:07 +0600

    Слияние веток клиента и тренера!

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import config from "./config";
 import * as mongoose from "mongoose";
 import usersRouter from "./routers/users";
+import clientsRouter from "./routers/clients";
+import trainersRouter from "./routers/trainers";
 
 const app = express();
 const port = 8000;
@@ -11,6 +13,8 @@ app.use(cors(config.corsOptions));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/users", usersRouter);
+app.use("/clients", clientsRouter);
+app.use("/trainers", trainersRouter);
 
 const run = async () => {
   await mongoose.connect(config.database);
--- models/Trainer.ts
+++ models/Trainer.ts
@@ -1,46 +1,46 @@
-import mongoose, { Types } from 'mongoose';
-import User from './User';
-import { TrainerTypes } from '../types/trainerTypes';
+import mongoose, { Types } from "mongoose";
+import User from "./User";
+import { TrainerTypes } from "../types/trainerTypes";
 
 const Schema = mongoose.Schema;
 
 const TrainerSchema = new Schema<TrainerTypes>({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-        unique: true,
-        validate: {
-            validator: async (value: Types.ObjectId) => {
-                const user = await User.findById(value);
-                return Boolean(user && user.role === "trainer");
-            },
-            message: "There can only be one role",
-        }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const user = await User.findById(value);
+        return Boolean(user && user.role === "trainer");
+      },
+      message: "There can only be one role",
     },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    timeZone:{
-        type: String,
-        required: true,
-    },
-    courseTypes:{
-        type: [String],
-    },
-    rating:{
-        type: Number,
-        default: 0,
-    },
-    avatar:{
-        type: String,
-    }
-})
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  timeZone: {
+    type: String,
+    required: true,
+  },
+  courseTypes: {
+    type: [String],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  avatar: {
+    type: String,
+  },
+});
 
-const Trainer = mongoose.model('Trainer',TrainerSchema);
+const Trainer = mongoose.model("Trainer", TrainerSchema);
 export default Trainer;
