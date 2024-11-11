cce812359bf2efac016ec4969f3139753997a65c
alyonaVet <a.v.alenka2015@gmail.com>
2024-11-11 22:43:51 +0600

    добавлены начальные данные для `LessonType` с двумя записями: "Yoga" и "Fitness";

--- fixtures.ts
+++ fixtures.ts
@@ -5,6 +5,7 @@ import Trainer from "./models/Trainer";
 import Client from "./models/Client";
 import Course from "./models/Course";
 import Lesson from "./models/Lesson";
+import LessonType from "./models/LessonType";
 
 const run = async () => {
   await mongoose.connect(config.database);
@@ -15,6 +16,7 @@ const run = async () => {
     await db.dropCollection("users");
     await db.dropCollection("courses");
     await db.dropCollection("lessons");
+    await db.dropCollection("lessontypes");
   } catch (err) {
     console.log("skipping drop");
   }
@@ -105,7 +107,17 @@ const run = async () => {
     groupLevel: 2,
     ageLimit: 21,
     description: "A session for advanced practitioners.",
-  })
+  });
+
+  await LessonType.create({
+    name: "Yoga",
+    description: "An introductory course on yoga.",
+    isPublished: false,
+  }, {
+    name: "Fitness",
+    description: "An introductory course on fitness.",
+    isPublished: false,
+  });
 
   await db.close();
 };
