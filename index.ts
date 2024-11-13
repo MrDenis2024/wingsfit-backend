b3ed8be9113cbcc9006a36728bf05a5bf0bf386a
Konstantin <115252606+koog7@users.noreply.github.com>
2024-11-13 13:56:12 +0600

    Изменен роутер и добавлены фикстуры

--- index.ts
+++ index.ts
@@ -7,9 +7,8 @@ import clientsRouter from "./routers/clients";
 import trainersRouter from "./routers/trainers";
 import coursesRouter from "./routers/courses";
 import lessonsRouter from "./routers/lessons";
-import { lessonTypeRouter } from "./routers/lessonTypes";
-import TrainerReview from "./models/TrainerReview";
-import { trainerReviewRouter } from "./routers/TrainerReview";
+import {lessonTypeRouter} from "./routers/lessonTypes";
+import {trainerReviewRouter} from "./routers/TrainerReview";
 
 const app = express();
 const port = 8000;
