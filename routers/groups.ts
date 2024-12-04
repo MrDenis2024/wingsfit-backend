4d741bca98ec37b21ac46c5a001fa2624488faba
Ivan Limarev <ivantaketo@gmail.com>
2024-12-04 13:34:30 +0600

    Group.ts, groups.ts, fixtures.ts. создания GET запроса на получение всех групп тренера, улучшение POST запроса на создание группы тренером. Обновление фикстурных данных групп

--- routers/groups.ts
+++ routers/groups.ts
@@ -1,17 +1,43 @@
 import express from "express";
 import Group from "../models/Group";
-import auth from "../middleware/auth";
+import auth, {RequestWithUser} from "../middleware/auth";
 import permit from "../middleware/permit";
 import Course from "../models/Course";
 import User from "../models/User";
 import mongoose from "mongoose";
+import Trainer from "../models/Trainer";
 
 export const groupsRouter = express.Router();
+
+groupsRouter.get("/", auth, async (req: RequestWithUser, res, next) => {
+  try {
+    const trainerId = req.query.trainer;
+
+    const trainer = await Trainer.findOne({user: trainerId});
+
+    if (!trainer) {
+      return res.status(400).send({ error: "Trainer not found" });
+    }
+
+    const groups = await Group.find()
+      .populate({
+        path: 'course',
+        match: { user: trainer.user },
+        select: 'title',
+      }).exec();
+
+    const filteredGroups = groups.filter((group) => group.course);
+
+    return res.send(filteredGroups);
+  } catch (error) {
+    return next(error);
+  }
+});
 
 groupsRouter.get("/:id", async (req, res, next) => {
   try {
     if (!mongoose.isValidObjectId(req.params.id))
-      return res.status(400).send({ error: "Invalid ID" });
+      return res.status(400).send({ error: "Invalid course ID" });
 
     const groups = await Group.find({ course: req.params.id }).populate(
       "clients",
@@ -23,22 +49,33 @@ groupsRouter.get("/:id", async (req, res, next) => {
   }
 });
 
-groupsRouter.post("/", auth, permit("trainer"), async (req, res, next) => {
+groupsRouter.post("/", auth, permit("trainer"), async (req: RequestWithUser, res, next) => {
   try {
-    const coursesExists = await Course.findById(req.query.course);
-    if (!coursesExists) {
+    const user = req.user;
+
+    if (!user) return res.status(400).send({ error: "User not found" });
+
+    const existingCourse = await Course.findOne({
+      _id: req.query.course,
+      user: user._id
+    });
+
+    if (!existingCourse) {
       return res.status(400).send({ error: "Course does not exist" });
     }
 
-    const newGroup = new Group({
+    if (!req.body.title || req.body.clientsLimit < 1 || !req.body.startTime || !req.body.trainingLevel) {
+      return res.status(400).send({ error: "Fill required fields!" });
+    }
+
+    const newGroup = await Group.create({
       title: req.body.title,
-      course: coursesExists._id,
-      clientsLimit: coursesExists.maxClients,
+      course: existingCourse._id,
+      clientsLimit: existingCourse.maxClients,
       startTime: req.body.startTime,
       trainingLevel: req.body.trainingLevel,
     });
 
-    await newGroup.save();
     return res.send(newGroup);
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
@@ -51,9 +88,9 @@ groupsRouter.post("/", auth, permit("trainer"), async (req, res, next) => {
 groupsRouter.patch("/:id", auth, permit("trainer"), async (req, res, next) => {
   try {
     if (!mongoose.isValidObjectId(req.params.id))
-      return res.status(400).send({ error: "Invalid ID" });
+      return res.status(400).send({ error: "Invalid group ID" });
 
-    if (!req.body.clientId) {
+    if (!mongoose.isValidObjectId(req.body.clientId)) {
       return res.status(400).send({ error: "Client ID is required" });
     }
 
@@ -62,13 +99,13 @@ groupsRouter.patch("/:id", auth, permit("trainer"), async (req, res, next) => {
       return res.status(400).send({ error: "Client does not exist" });
     }
 
+    if (client.role !== "client") {
+      return res.status(400).send({ error: "You can only add a client" });
+    }
+
     const group = await Group.findById(req.params.id);
     if (!group) {
       return res.status(400).send({ error: "Group does not exist" });
-    }
-
-    if (client.role !== "client") {
-      return res.status(400).send({ error: "You can only add a client" });
     }
 
     if (group.clients.includes(client._id)) {
