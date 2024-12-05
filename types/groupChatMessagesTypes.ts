29bb22f7b11687ac1091eabfb4a4ef46a655af52
alyonaVet <a.v.alenka2015@gmail.com>
2024-12-05 12:12:38 +0600

    добавлены типы IncomingMessage, ConnectedClients;

--- types/groupChatMessagesTypes.ts
+++ types/groupChatMessagesTypes.ts
@@ -1,4 +1,5 @@
 import { Types } from "mongoose";
+import {WebSocket} from "ws";
 
 export interface GroupChatMessages {
   groupChat: Types.ObjectId | string;
@@ -6,4 +7,31 @@ export interface GroupChatMessages {
   message: string;
   createdAt: Date;
   isRead: Array<{ user: string; read: boolean }>;
+}
+
+export type IncomingMessage =
+  | {
+  type: "LOGIN";
+  payload: string;
+}
+  | {
+  type: "JOIN_GROUP";
+  payload: { groupChatId: string };
+}
+  | {
+  type: "SEND_MESSAGE";
+  payload: { groupChatId: string; message: string };
+
+} | {
+  type: "MARK_READ";
+  payload: { messageId: string; groupChatId: string };
+};
+
+
+export interface ConnectedClients {
+  [userId: string]: {
+    userName: string;
+    clients: WebSocket[];
+    groups: string[];
+  }
 }
