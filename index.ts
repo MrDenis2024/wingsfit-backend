d47a630eb297e9749c715e5172950aff79474652
alyonaVet <a.v.alenka2015@gmail.com>
2024-12-13 23:33:30 +0600

    добавлен route privateChats;

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ import adminsRouter from "./routers/admins";
 import groupsRouter from "./routers/groups";
 import trainerStatisticsRouter from "./routers/trainerStatistics";
 import createGroupChatRouter from "./routers/groupChats";
+import createPrivateChatRouter from "./routers/privateChats";
 
 const app = express();
 const port = 8000;
@@ -34,6 +35,7 @@ app.use("/admins", adminsRouter);
 app.use("/groups", groupsRouter);
 app.use("/trainerStatistics", trainerStatisticsRouter);
 app.use("/groupChats", createGroupChatRouter());
+app.use("/privateChats", createPrivateChatRouter());
 
 const run = async () => {
   if (!config.database) {
