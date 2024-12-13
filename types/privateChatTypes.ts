1177c575e43018dd5d8c830f1a39d6eb2d8663cd
alyonaVet <a.v.alenka2015@gmail.com>
2024-12-13 23:34:21 +0600

    добавлен interface для ConnectedClientsToPrivateChat;

--- types/privateChatTypes.ts
+++ types/privateChatTypes.ts
@@ -1,7 +1,17 @@
 import { Types } from "mongoose";
+import { WebSocket } from "ws";
+
 
 export interface PrivateChatTypes {
   firstPerson: Types.ObjectId | string;
   secondPerson: Types.ObjectId | string;
   availableTo: [Types.ObjectId | string, Types.ObjectId | string];
+}
+
+export interface ConnectedClientsToPrivateChat {
+  [userId: string]: {
+    userName: string;
+    clients: WebSocket[];
+    privateChats: string[];
+  };
 }
