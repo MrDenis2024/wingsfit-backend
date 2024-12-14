d334b4a4bfb6262b9499781f10c65a1d9799a7dc
Khrunev Denis <deniskhrunev@gmail.com>
2024-12-14 18:13:27 +0600

    Создана структруа роутов для статистики отмененных или отписок от записей WIN-106

--- routers/trainerStatistics.ts
+++ routers/trainerStatistics.ts
@@ -3,6 +3,9 @@ import Group from "../models/Group";
 import auth, { RequestWithUser } from "../middleware/auth";
 import Course from "../models/Course";
 import permit from "../middleware/permit";
+import CourseRequest from "../models/CourseRequest";
+import CoursesRequest from "./coursesRequest";
+import {ICourseRequest} from "../types/courseTypes";
 
 export const trainerStatisticsRouter = express.Router();
 
@@ -74,6 +77,152 @@ trainerStatisticsRouter.get(
       ]);
 
       return res.status(200).send(clientStats);
+    } catch (error) {
+      return next(error);
+    }
+  },
+);
+
+trainerStatisticsRouter.get(
+  "/cancellations",
+  auth,
+  permit("trainer"),
+  async (req: RequestWithUser, res, next) => {
+    try {
+      const { startDate, endDate } = req.query;
+
+      const validStartDate =
+        startDate && typeof startDate === "string" ? new Date(startDate) : null;
+      const validEndDate =
+        endDate && typeof endDate === "string" ? new Date(endDate) : null;
+
+      if (validStartDate && isNaN(validStartDate.getTime())) {
+        return res.status(400).send({ error: "Invalid startDate format" });
+      }
+
+      if (validEndDate && isNaN(validEndDate.getTime())) {
+        return res.status(400).send({ error: "Invalid endDate format" });
+      }
+
+      const courses = await Course.find({ user: req.user?._id });
+      const courseIds = courses.map((course) => course._id);
+
+      const cancellations: ICourseRequest[] = await CourseRequest.aggregate([
+        {
+          $match: {
+            course: { $in: courseIds },
+            status: "declined",
+            ...(validStartDate && { updatedAt: { $gte: validStartDate } }),
+            ...(validEndDate && { updatedAt: { $lte: validEndDate } }),
+          },
+        },
+      ]);
+      console.log(cancellations.length);
+
+      return res.send({
+        canceled: cancellations.length,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  },
+);
+
+trainerStatisticsRouter.get(
+  "/unsubscriptions",
+  auth,
+  permit("trainer"),
+  async (req: RequestWithUser, res, next) => {
+    try {
+      const { startDate, endDate } = req.query;
+
+      const validStartDate =
+        startDate && typeof startDate === "string" ? new Date(startDate) : null;
+      const validEndDate =
+        endDate && typeof endDate === "string" ? new Date(endDate) : null;
+
+      if (validStartDate && isNaN(validStartDate.getTime())) {
+        return res.status(400).send({ error: "Invalid startDate format" });
+      }
+
+      if (validEndDate && isNaN(validEndDate.getTime())) {
+        return res.status(400).send({ error: "Invalid endDate format" });
+      }
+
+      const courses = await Course.find({ user: req.user?._id });
+      const courseIds = courses.map((course) => course._id);
+
+      const unsubscriptions: ICourseRequest[] = await CourseRequest.aggregate([
+        {
+          $match: {
+            course: { $in: courseIds },
+            status: "unsubscribed",
+            ...(validStartDate && { updatedAt: { $gte: validStartDate } }),
+            ...(validEndDate && { updatedAt: { $lte: validEndDate } }),
+          },
+        },
+      ]);
+
+      return res.send({
+        unsubscribed: unsubscriptions.length,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  },
+);
+
+trainerStatisticsRouter.get(
+  "/cancellations-unsubscriptions",
+  auth,
+  permit("trainer"),
+  async (req: RequestWithUser, res, next) => {
+    try {
+      const { startDate, endDate } = req.query;
+
+      const validStartDate =
+        startDate && typeof startDate === "string" ? new Date(startDate) : null;
+      const validEndDate =
+        endDate && typeof endDate === "string" ? new Date(endDate) : null;
+
+      if (validStartDate && isNaN(validStartDate.getTime())) {
+        return res.status(400).send({ error: "Invalid startDate format" });
+      }
+
+      if (validEndDate && isNaN(validEndDate.getTime())) {
+        return res.status(400).send({ error: "Invalid endDate format" });
+      }
+
+      const courses = await Course.find({ user: req.user?._id });
+      const courseIds = courses.map((course) => course._id);
+
+      const [cancellations, unsubscriptions] = await Promise.all<ICourseRequest[]>([
+        CourseRequest.aggregate([
+          {
+            $match: {
+              course: { $in: courseIds },
+              status: "declined",
+              ...(validStartDate && { updatedAt: { $gte: validStartDate } }),
+              ...(validEndDate && { updatedAt: { $lte: validEndDate } }),
+            },
+          },
+        ]),
+        CourseRequest.aggregate([
+          {
+            $match: {
+              course: { $in: courseIds },
+              status: "unsubscribed",
+              ...(validStartDate && { updatedAt: { $gte: validStartDate } }),
+              ...(validEndDate && { updatedAt: { $lte: validEndDate } }),
+            },
+          },
+        ]),
+      ]);
+
+      return res.send({
+        canceled: cancellations.length,
+        unsubscribed: unsubscriptions.length,
+      });
     } catch (error) {
       return next(error);
     }
