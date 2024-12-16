8b0922a8ef48fb54add618dcc0aea9d55318628b
Khrunev Denis <deniskhrunev@gmail.com>
2024-12-16 23:20:48 +0600

    Деплой на сервер из develop ветки

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+    apps : [{
+        name   : "backend",
+        script : "/home/wingsfit-admin/current/backend/index.ts",
+        instances: 1,
+        env: {
+            DATABASE_URL: "mongodb://localhost/wingsfit",
+            CORS_WHITELIST: "http://209.38.96.160",
+            GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+            GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+            NODE_ENV: "production"
+        }
+    }]
+};
