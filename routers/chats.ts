e1927d50d89222f84191cc30a2fb7359ecb9be34
alyonaVet <a.v.alenka2015@gmail.com>
2025-01-10 21:58:52 +0600

    создан маршрут для создания приватного чата, который проверяет, существует ли чат, поэтому новый чат не создается, но если чата с двумя пользователями нет, то создается новый;

--- routers/chats.ts
+++ routers/chats.ts
@@ -4,6 +4,7 @@ import Group from "../models/Group";
 import GroupChat from "../models/GroupChat";
 import Course from "../models/Course";
 import PrivateChat from "../models/PrivateChat";
+import User from "../models/User";
 
 const chatsRouter = express.Router();
 
@@ -71,5 +72,54 @@ chatsRouter.get(
     }
   },
 );
+
+chatsRouter.post("/start-chat", async (req, res) => {
+  const { firstPersonId, secondPersonId } = req.body;
+
+  try {
+    const firstPerson = await User.findById(firstPersonId);
+    const secondPerson = await User.findById(secondPersonId);
+
+    if (!firstPerson || !secondPerson) {
+      return res.status(400).send({ error: "One or both users not found" });
+    }
+
+    if (firstPerson._id.toString() === secondPerson._id.toString()) {
+      return res
+        .status(400)
+        .send({ error: "First and second persons cannot be the same" });
+    }
+
+    if (!["trainer", "admin", "superAdmin"].includes(firstPerson.role)) {
+      return res.status(400).send({
+        error:
+          "First person must have a valid role (trainer, admin, or superAdmin)",
+      });
+    }
+
+    const existingChat = await PrivateChat.findOne({
+      $or: [
+        { firstPerson: firstPerson._id, secondPerson: secondPerson._id },
+        { firstPerson: secondPerson._id, secondPerson: firstPerson._id },
+      ],
+    });
+
+    if (existingChat) {
+      return res.status(200).send(existingChat);
+    }
+
+    const newPrivateChat = new PrivateChat({
+      firstPerson: firstPerson._id,
+      secondPerson: secondPerson._id,
+      availableTo: [firstPerson._id, secondPerson._id],
+    });
+
+    await newPrivateChat.save();
+
+    return res.status(201).send(newPrivateChat);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
 
 export default chatsRouter;
