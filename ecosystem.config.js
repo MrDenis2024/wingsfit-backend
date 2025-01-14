893b57ef24ab013e17d2b66ce69a306b58999728
MrDenis2024 <deniskhrunev@gmail.com>
2025-01-14 22:50:59 +0600

    Изменен whitelist (#80)
    * Изменен whitelist
    
    * Измененя

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       instances: 1,
       env: {
         DATABASE_URL: "mongodb://localhost/wingsfit",
-        CORS_WHITELIST: "http://209.38.96.160",
+        CORS_WHITELIST: "http://wingsfit.publicvm.com",
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         NODE_ENV: "production",
